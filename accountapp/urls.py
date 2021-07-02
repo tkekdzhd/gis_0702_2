b00935d3ce0c53e3170d1569f89ade99fa1d4721
Cheon Gayoung <tkekdzhd@gmail.com>
2021-07-02 12:14:34 +0900

    0702 commit 2

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path, include
+from django.urls import path
 from accountapp.views import hello_world
 
-app_name="accountapp"
+app_name = 'accountapp'
 urlpatterns = [
-    path('hello_world/', hello_world, name="hello_world"),
+    path('hello_world/', hello_world, name='hello_world'),
 ]
--- djangoProject1/urls.py
+++ djangoProject1/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('account/', include('accountapp.urls'))
+    path('accounts/', include('accountapp.urls'))
 ]
