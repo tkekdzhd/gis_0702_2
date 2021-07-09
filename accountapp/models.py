f710d2cead682189bd79ff315a0b463e3162e743
Cheon Gayoung <tkekdzhd@gmail.com>
2021-07-09 11:22:35 +0900

    0709 commit 1

--- accountapp/models.py
+++ accountapp/models.py
@@ -1,3 +1,5 @@
-from django.db import models
+from django.db import models #models는 패키지명
 
 # Create your models here.
+class NewModel(models.Model):
+    text = models.CharField(max_length=255, null=False)
