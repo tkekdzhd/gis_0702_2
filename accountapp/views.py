9c1e05394b8b2fa94e638bfec48d04422c58aaff
Cheon Gayoung <tkekdzhd@gmail.com>
2021-07-13 12:41:03 +0900

    0713 commit 3

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,8 +1,12 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 
 from accountapp.models import NewModel
+from django.views.generic import CreateView
+
 
 # Create your views here.
 
@@ -19,3 +23,9 @@ def hello_world(request):
     else:
         data_list = NewModel.objects.all()
         return render(request, 'accountapp/hello_world.html', context={'data_list': data_list})
+
+    class AccountCreateView(CreateView):
+        model = User
+        form_class = UserCreationForm
+        success_url = reverse_lazy('accountapp:hello_world')
+        template_name = 'accountapp/create.html'
