2e47f1bf270091a4199a009f226cf53d16d98909
Cheon Gayoung <tkekdzhd@gmail.com>
2021-07-14 17:46:29 +0900

    0714 commit 2

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,9 +1,13 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from accountapp.views import hello_world, AccountCreateView
 
 app_name = 'accountapp'
 urlpatterns = [
     path('hello_world/', hello_world, name='hello_world'),
+    path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+
     # 클래스에는 .as_view 필요
     path('create/', AccountCreateView.as_view(), name='create'),
 ]
--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -15,6 +15,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # __file__  = settings.py 가 있는 프로젝트 폴더를 가르킴
 # parent.parent = 프로젝트 폴더 (settings.py가 있는 위치의 부모의 부모)
+from django.urls import reverse_lazy
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 env_list = dict()
@@ -124,7 +126,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
 TIME_ZONE = 'UTC'
 
@@ -148,3 +150,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
+LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
