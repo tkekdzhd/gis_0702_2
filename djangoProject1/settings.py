81b867ada33805d33bed2567cfd50442f8bbdaf6
Cheon Gayoung <tkekdzhd@gmail.com>
2021-07-16 11:24:10 +0900

    0716 commit 1

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -58,7 +58,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accountapp'
+    'accountapp',
+    'bootstrap4'
 ]
 
 MIDDLEWARE = [
