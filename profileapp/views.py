881d02f71e39ebefa49a662c32d7d28da9f2fa50
Cheon Gayoung <tkekdzhd@gmail.com>
2021-07-27 12:25:24 +0900

    0727 commit 3

--- profileapp/views.py
+++ profileapp/views.py
@@ -7,8 +7,14 @@ from profileapp.forms import ProfileCreationForm
 from profileapp.models import Profile
 
 
+
 class ProfileCreateView(CreateView):
     model = Profile
     form_class = ProfileCreationForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/create.html'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
