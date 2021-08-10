4b45abf22d8de08e9cdc4ee0c798021112777642
Cheon Gayoung <tkekdzhd@gmail.com>
2021-08-10 12:47:46 +0900

    0810 commit 5

--- commentapp/views.py
+++ commentapp/views.py
@@ -11,5 +11,11 @@ class CommentCreateView(CreateView):
     form_class =CommentCreationForm
     template_name = 'commentapp/create.html'
 
+    # 입력받은 폼 검증, 검증 후 최종적으로 불려지는 폼
+    def form_valid(self, form):
+        form.instance.writer = self.request.user
+        form.instance.article_id = self.request.POST.get('article_pk')
+        return super().form_valid(form)
+
     def get_success_url(self):
         return reverse('articleapp:detail', kwargs={'pk':self.object.article.pk})
