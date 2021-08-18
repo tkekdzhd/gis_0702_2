4e1e8521516deacc5e0a4c09853604f3045c3645
Cheon Gayoung <tkekdzhd@gmail.com>
2021-08-18 17:07:00 +0900

    0818 commit 2

--- articleapp/models.py
+++ articleapp/models.py
@@ -2,12 +2,16 @@ from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from projectapp.models import Project
+
 
 class Article(models.Model):
     # on_delete = models.CASCADE: 종속, 유저가 삭제되면 article도 삭제된다
     # on_delete = models.SET_NULL: 유저가 삭제되도 게시글은 남아있으며 작성자명은 null로 된다.
 
     writer = models.ForeignKey(User, on_delete=models.SET_NULL,
+                               related_name='article', null=True)
+    project = models.ForeignKey(Project, on_delete=models.SET_NULL,
                                related_name='article', null=True)
     title = models.CharField(max_length=200, null=True)
     image = models.ImageField(upload_to='article/', null=True)
